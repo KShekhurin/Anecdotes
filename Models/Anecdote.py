bd9f209a1cf17a350cded5972acb0d4c6cabf9f1
Kirill Shekhurin <k.shekhurin@gmail.com>
2022-05-10 19:07:21 +0300

    Removed rating (must be done another way)

--- Models/Anecdote.py
+++ Models/Anecdote.py
@@ -6,14 +6,13 @@ class Anecdote:
         return json.dumps(self, default=lambda o: o.__dict__,
                           sort_keys=True, indent=4)
 
-    def __init__(self, id=None, text="", rating=0, in_queue=False):
+    def __init__(self, id=None, text="", in_queue=False):
         self.id = id
         self.text = text
-        self.rating = rating
         self.in_queue = in_queue
 
     @staticmethod
     def build_from_tuple(tuple):
         if tuple is None:
             return None
-        return Anecdote(tuple[0], tuple[1], tuple[2], tuple[3])
+        return Anecdote(tuple[0], tuple[1], tuple[2])
